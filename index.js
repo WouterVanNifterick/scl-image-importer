df5afbeb39362e4e5c4a2d0f70543037018e2a82
Wouter van Nifterick <wouter@ars.nl>
2019-10-30 22:41:09 +0100

    Make fully async, save SQL

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 const fs = require('fs');
 var mysql = require('mysql');
+let allSQL=[];
 
-const walk = function(dir, ext) {
+const newConnection = () => {
+    let dbConn = mysql.createConnection({
+        host: '10.4.1.23',
+        user: 'scl_api.user',
+        password: 'scl',
+        database: 'scapeye',
+        multipleStatements: true
+    });
+    dbConn.connect(); 
+    return dbConn;
+}
+
+const walk = async (dir, ext, level) => {
     let results = [];
-    console.log(dir);
+    level++;
+    console.log(`level ${level}`, dir );
+
     const list = fs.readdirSync(dir);
     list.forEach(function(file) {
         file = dir + '/' + file;
         const stat = fs.statSync(file);
         if (stat && stat.isDirectory()) { 
-            results = results.concat(walk(file, ext));
+            results = results.concat(walk(file, ext, level));
         } else { 
             file_type = file.split(".").pop();
             file_name = file.split(/(\\|\/)/g).pop();
@@ -43,40 +58,58 @@ const walk = function(dir, ext) {
     // var sql=[];
     let insertSQL = [];
     results.forEach(f=>{
-        insertSQL.push(`('${f.filename.replace(startDir,'') }', '${f.timestamp.toISOString().replace("T"," ").replace("Z","")}', '${f.lp.replace(/ /g,"")}', '${f.country}', ${f.len}, ${f.conf}, ${f.coords.topleftx}, ${f.coords.toplefty}, ${f.coords.toprightx}, ${f.coords.toprighty}, ${f.coords.bottomrightx}, ${f.coords.bottomrighty}, ${f.coords.bottomleftx}, ${f.coords.bottomlefty}, '${f.classifier_rdwclass}')`);
+        try{
+            if(f.filename)
+                insertSQL.push(`(${imagesSetId}, '${f.filename.replace(startDir,'') }', '${f.timestamp.toISOString().replace("T"," ").replace("Z","")}', '${f.lp.replace(/ /g,"")}', '${f.country}', ${f.len}, ${f.conf}, ${f.coords.topleftx}, ${f.coords.toplefty}, ${f.coords.toprightx}, ${f.coords.toprighty}, ${f.coords.bottomrightx}, ${f.coords.bottomrighty}, ${f.coords.bottomleftx}, ${f.coords.bottomlefty}, '${f.classifier_rdwclass}')`);
+        }
+        catch (e){
+            console.error(e);
+        }
+        
     });
-    insertSQL = `INSERT IGNORE INTO scl_image (filename, timestamp, lp, country, len, conf, topleftx, toplefty, toprightx, toprighty, bottomrightx, bottomrighty, bottomleftx, bottomlefty, classifier_rdwclass) VALUES ` + insertSQL.join(", \n");
-    console.log(insertSQL);
-    dbConn.connect(); 
-    dbConn.query(insertSQL, [], function (error, results, fields) {
-        if (error) {
-          console.log(error);
-//          return;
-        }        
-        console.log(results);
-        dbConn.commit();
-    });
+    if(insertSQL.length>0)
+    {
+        insertSQL = `INSERT IGNORE INTO scl_image (scl_imagesetid, filename, timestamp, lp, country, len, conf, topleftx, toplefty, toprightx, toprighty, bottomrightx, bottomrighty, bottomleftx, bottomlefty, classifier_rdwclass) VALUES ` + insertSQL.join(", \n");
+        allSQL.push(insertSQL);
+    }
+    level--;
+    if(level===0){
+        // console.log(allSQL);
+        const sql = allSQL.join(";\n\n");
+        fs.writeFile(startDir+"/insert.sql", sql, e=>{console.error(e||"Save OK"); 
 
-    // var allSQL=sql.join("\n");
-    // var fileName=startDir+"/import.sql";
-    // 
-    // fs.appendFile(fileName, allSQL, function(err) {
-    //     if(err) {
-    //         return console.log(err);
-    //     }
-    //     console.log(`${fileName} was saved!`);
-    // }); 
+        var dbConn = newConnection();
+        dbConn.query(sql, [], function (error, results, fields) {
+            console.log(error||"Inserts OK");          
+            // console.log(results);
+            dbConn.commit(err=>
+                {
+                    console.error(err||"Commit OK");
+                    dbConn.end();
+                }
+                );        
+        });
+    }); 
+    }
     return results;
 }
-
-  
-// connect to database
-var dbConn = mysql.createConnection({
-    host: '10.4.1.23',
-    user: 'scl_api.user',
-    password: 'scl',
-    database: 'scapeye'
-});
-//dbConn.connect(); 
+ 
 const startDir = process.argv[2];
-walk(startDir,'json');
+let imagesSetId = null;
+function main(){
+    var dbConn = newConnection(); 
+    dbConn.query('insert ignore into scl_imageset(scl_imageset,basepath) values (?,?)', [startDir,startDir], function (error, results, fields) {
+        console.error(error||"Insert OK");
+        imagesSetId = results?results.insertId:5;
+        dbConn.commit( error=>{
+            console.error(error||"Commit OK");
+            dbConn.end(e=>{console.error(e||"Destruct conn OK"); 
+                           walk(startDir,'json',0);});
+            
+        }
+        );
+    });
+}
+
+
+main();
